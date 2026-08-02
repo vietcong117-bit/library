66d9b115a072f5403632cd3feebf7b0f3f08f48a
Trương Hữu An <101454418+truonghuuanso@users.noreply.github.com>
2026-08-02 16:58:22 +0700

    Update ai_brain.py

--- ai_brain.py
+++ ai_brain.py
@@ -44,7 +44,7 @@ def get_chatbot_response(user_message):
 STOPWORDS_SEARCH = {
     "tìm", "kiếm", "sách", "cuốn", "truyện", "quyển", "giúp", "mình",
     "muốn", "hỏi", "cho", "về", "có", "là", "tên", "đọc", "mượn",
-    "vậy", "không", "bạn", "ơi", "nhé", "giùm", "dùm", "ạ"
+    "vậy", "không", "bạn", "ơi", "nhé", "giùm", "dùm", "ạ","?","!",".",","
 }
 
 MIN_QUERY_LENGTH = 2  
@@ -88,7 +88,7 @@ STOPWORDS_AUTHOR = {
     "tìm", "sách", "cuốn", "truyện", "quyển", "tác", "phẩm", "tác_phẩm",
     "giả", "tác_giả", "của", "do", "viết", "bởi", "cho", "mình", "muốn",
     "xem", "hỏi", "có", "không", "là", "gì", "này", "những", "các",
-    "liệt", "kê", "liệt_kê", "giúp", "nào", "nhé", "ạ","của","bạn"
+    "liệt", "kê", "liệt_kê", "giúp", "nào", "nhé", "ạ","của","bạn","?","!",".",","
 }
 
 
@@ -126,7 +126,7 @@ STOPWORDS_CATEGORY = {
     "tìm", "sách", "cuốn", "truyện", "quyển", "thể", "loại", "thể_loại",
     "thuộc", "cho", "mình", "muốn", "xem", "hỏi", "có", "không", "là",
     "gì", "này", "những", "các", "liệt", "kê", "liệt_kê", "giúp", "nào",
-    "nhé", "ạ","thuộc"
+    "nhé", "ạ","thuộc","?","!",".",","
 }
 
 
